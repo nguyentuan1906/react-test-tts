494d38283deb6a0ac575f17d17763f7af8e27736
nguyentuan1906 <roccat.nguyentuan@gmail.com>
2023-02-15 14:33:46 +0700

    start

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../component/NavBar'
+
+const Home = () => {
+  return (
+    <div className={styles[home]}>
+
+    </div>
+  )
+}
+
+export default Home
