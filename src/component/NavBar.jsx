f1b77217d4e6075c1849c5b8c0bd51e624fa78e1
nguyentuan1906 <roccat.nguyentuan@gmail.com>
2023-02-15 23:35:19 +0700

    header

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import style from './NavBar.module.css'
-import TopLeftShape from '../assets/Top-Left-Shape.png'
 import Finanzo from '../assets/Finanzo.png'
+import SettingsPhoneTwoToneIcon from '@mui/icons-material/SettingsPhoneTwoTone';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const NavBar = () => {
     return (
@@ -17,12 +20,36 @@ const NavBar = () => {
                     <a href="">Pages</a>
                     <a href="">Team</a>
                     <button>Contact Us</button>
-                    <div className="ContactBox">
-                        This contact
+                    <div className={style['ContactBox']}>
+                        <SettingsPhoneTwoToneIcon className={style['icon']} fontSize="large" />
+                        <div>
+                            <p>Call us at:</p>
+                            <p >+613 8376 628</p>
+                        </div>
                     </div>
                 </div>
-
             </div>
+            <div className={style['Banner']}>
+                <div className={style['BannerLink']}>
+                    <a href=""> <FacebookOutlinedIcon fontSize="large" className={style['Link']} /> </a>
+                    <a href=""> <TwitterIcon fontSize="large" className={style['Link']} /> </a>
+                    <a href=""> <LinkedInIcon fontSize="large" className={style['Link']} /> </a>
+                </div>
+                <div className={style['BannerContent1']}>
+                    <div className={style['Curve']}></div>
+                    <div className={style['BannerContent']}>
+                        <h1>Create New Ways to Better Invest</h1>
+                        <p>Duis aute irure dolor in reprehenderit in voluptate velit cillum
+                            nulla pariatur nostrud exercitation.</p>
+                    </div>
+                    <div data-aos="fade-down" className={style['BannerBtn']}>
+                        <button className={style['btn1']}>Get Start</button>
+                        <button>Learn More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="none"></div>
+            <div className={style['BottomRightShape']}></div>
         </div>
     )
 }
