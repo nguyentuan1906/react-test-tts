64be05da118c463d0beb776433245d5365fcb078
nguyentuan1906 <roccat.nguyentuan@gmail.com>
2023-02-16 15:48:53 +0700

    AboutComplete

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -1,35 +1,11 @@
 import React from 'react'
 import style from './Container.module.css'
-import AboutSecImg from '../assets/about-sec-img.jpg'
-import video from '../assets/video-poster.jpg'
+import AboutUs from './AboutUs'
 
 const Container = () => {
     return (
         <div className={style['Container']}>
-            <div className="about">
-                <div className={style['AboutSection']}>
-                    <div className={style['about-left-con']}>
-                        <div className={style['TopLeftShape']}></div>
-                        <div className={style['about-sec-img']}>
-                            <img src={AboutSecImg} alt="" />
-                        </div>
-                    </div>
-                    <div className={style['about-right-con']}>
-                        <h5 className={style['ab-title']}>About us</h5>
-                        <h2>We are The Best Finance
-                            Agency in the Area</h2>
-                        <h5 className={style['ab-content']}>Duis aute irure dolor in reprehenderit in voluptate velit cillum nulla pae
-                            nostrud exercitation alias consequatur aut perferendis doloribus asperi
-                            aut rerum necessitatibus saeo eveniet.
-                        </h5>
-                        <h5 className={style['ab-content']}>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihio
-                            quo minus maxime placeat facere.</h5>
-                    </div>
-                </div>
-                <div className={style['ab-video']}>
-                    <img src={video} alt="" />
-                </div>
-            </div>
+            <AboutUs/>
         </div>
     )
 }
