1e61d7f2c2358d255e3e0d194243fb63f9a9dad1
nguyentuan1906 <roccat.nguyentuan@gmail.com>
2023-02-16 16:21:01 +0700

    .

--- src/component/AboutUs.jsx
+++ src/component/AboutUs.jsx
@@ -5,29 +5,33 @@ import video from '../assets/video-poster.jpg'
 
 const AboutUs = () => {
     return (
-        <div><div className={style['AboutSection']}>
-            <div className={style['about-left-con']}>
-                <div className={style['TopLeftShape']}></div>
-                <div className={style['about-sec-img']}>
-                    <img src={AboutSecImg} alt="" />
+        <div className={style['AboutUs']}>
+            <div className={style['AboutSection']}>
+                <div className={style['about-left-con']}>
+                    <div className={style['TopLeftShape']}></div>
+                    <div className={style['about-sec-img']}>
+                        <img src={AboutSecImg} alt="" />
+                    </div>
+                </div>
+                <div className={style['about-right-con']}>
+                    <h5 className={style['ab-title']}>About us</h5>
+                    <h2>We are The Best Finance
+                        Agency in the Area</h2>
+                    <h5 className={style['ab-content']}>Duis aute irure dolor in reprehenderit in voluptate velit cillum nulla pae
+                        nostrud exercitation alias consequatur aut perferendis doloribus asperi
+                        aut rerum necessitatibus saeo eveniet.
+                    </h5>
+                    <h5 className={style['ab-content']}>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihio
+                        quo minus maxime placeat facere.</h5>
                 </div>
             </div>
-            <div className={style['about-right-con']}>
-                <h5 className={style['ab-title']}>About us</h5>
-                <h2>We are The Best Finance
-                    Agency in the Area</h2>
-                <h5 className={style['ab-content']}>Duis aute irure dolor in reprehenderit in voluptate velit cillum nulla pae
-                    nostrud exercitation alias consequatur aut perferendis doloribus asperi
-                    aut rerum necessitatibus saeo eveniet.
-                </h5>
-                <h5 className={style['ab-content']}>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihio
-                    quo minus maxime placeat facere.</h5>
-            </div>
-        </div>
             <div className={style['ab-video']}>
                 <div className={style['ab-video-none']}></div>
-                <img src={video} alt="" />
-            </div></div>
+                <div className={style['ab-video-none']}>
+                    <img src={video} alt="" />
+                </div>
+            </div>
+        </div>
     )
 }
 
