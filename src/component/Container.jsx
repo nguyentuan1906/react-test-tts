2805214dc34cd01485b9bbeef3a6b71d7a192a5f
nguyentuan1906 <roccat.nguyentuan@gmail.com>
2023-02-16 22:59:21 +0700

    Business

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import style from './Container.module.css'
 import AboutUs from './AboutUs'
 import Service from './Service'
+import Business from './Business'
 const Container = () => {
     return (
         <div className={style['Container']}>
             <AboutUs/>
             <Service/>
+            <Business/>
         </div>
     )
 }
